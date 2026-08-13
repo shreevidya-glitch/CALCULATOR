de82678f529d101f5a2b51aaae488178d71057c1
Chakali Shreevidya <chakalishreevidya@gmail.com>
2026-08-13 10:39:23 +0530

    Add files via upload

---
+++ project-31/Calculator.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("===== SIMPLE CALCULATOR =====");
+
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+
+        double result = 0;
+
+        if (op == '+') {
+            result = a + b;
+        } 
+        else if (op == '-') {
+            result = a - b;
+        } 
+        else if (op == '*') {
+            result = a * b;
+        } 
+        else if (op == '/') {
+            if (b != 0) {
+                result = a / b;
+            } else {
+                System.out.println("Cannot divide by zero.");
+                return;
+            }
+        } 
+        else {
+            System.out.println("Invalid operator.");
+            return;
+        }
+
+        System.out.println("Result = " + result);
+
+        sc.close();
+    }
+}
